f1352c17f39d2232e07fe603ded6783fc8491162
aptakqmf12 <aptakqmf12@gmail.com>
2023-05-29 21:29:01 +0900

    db 커넥트

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: "",
-});
+const uuid = require("uuid");
+const db = require("./db/index.ts");
 
 const PORT = 3001;
 const app = express();
@@ -19,51 +14,42 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  console.log("req : ", req);
-  res.send("hellow world");
-});
+app.get("/", (req, res) => {});
 
-let userList = [
-  { id: 1, name: "김태완", old: 31 },
-  { id: 2, name: "이도경", old: 30 },
-  { id: 3, name: "장재형", old: 30 },
-];
-
+//get
 app.get("/users", (req, res) => {
-  res.json(userList);
-  console.log(req.params);
+  db.query(`SELECT * FROM crud`, (err, results, fields) => {
+    if (err) console.log(err);
+
+    res.json(results);
+  });
 });
 
+//create
 app.post("/users/create", (req, res) => {
   if (!req.body) return;
-
-  const id = userList.length + 1;
   const { name, old } = req.body;
-  userList.push({ id, name, old });
-
-  res.send("user added.");
+  db.query(
+    `INSERT INTO crud (id, name, old) VALUES ("${uuid.v4()}", "${name}", ${old})`
+  );
+  res.send(`${name} 유저 생성`);
 });
 
+//remove
 app.post("/users/remove", (req, res) => {
   if (!req.body) return;
   const id = req.body.id;
+  db.query(`DELETE FROM crud WHERE id = "${id}"`);
 
-  userList = userList.filter((user) => user.id !== id);
-
-  res.send("user added.");
+  res.send("유저 삭제");
 });
 
+//update
 app.put("/users/update", (req, res) => {
   const { id, name, old } = req.body;
-
-  userList.map((user) => {
-    if (user.id === id) {
-      user.name = name;
-      user.old = old;
-    }
-    return user;
-  });
+  db.query(
+    `UPDATE crud SET name = "${name}", old = ${old} WHERE id = "${id}" `
+  );
   res.send("user updated.");
 });
 
