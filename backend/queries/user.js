ef6bfa95581781cf04d05386de4e66f181fe8b0d
aptakqmf12 <aptakqmf12@gmail.com>
2023-07-23 15:49:42 +0900

    backend query 분기

---
+++ backend/queries/user.js
@@ -0,0 +1,10 @@
+const getUsers = () => `SELECT * FROM crud`;
+
+const createUser = (id, name, old) =>
+  `INSERT INTO crud (id, name, old) VALUES ("${id}", "${name}", ${old})`;
+
+const removeUser = (id) => `DELETE FROM crud WHERE id = "${id}"`;
+const updateUser = (id, name, old) =>
+  `UPDATE crud SET name = "${name}", old = ${old} WHERE id = "${id}" `;
+
+module.exports = { getUsers, createUser, removeUser, updateUser };
