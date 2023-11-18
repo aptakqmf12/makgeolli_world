36c18eaf446ce33ec4431eaa3ff9db5f9038ebab
aptakqmf12 <aptakqmf12@gmail.com>
2023-11-18 21:32:47 +0900

    input, select, pie차트 컴포넌트 추가

--- front/src/component/common/input.tsx
+++ front/src/component/common/input.tsx
@@ -4,16 +4,25 @@ import { TextField } from "@mui/material";
 interface CommonInputProps {
   value: string;
   setValue: (v: string) => void;
+  variant?: "filled" | "outlined" | "standard";
+  fullWidth?: boolean;
 }
 
-export default function CommonInput({ value, setValue }: CommonInputProps) {
+export default function CommonInput({
+  value,
+  setValue,
+  variant = "outlined",
+  fullWidth = false,
+}: CommonInputProps) {
   return (
     <TextField
-      variant="standard"
+      variant={variant}
+      size="small"
       value={value}
       onChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
         setValue(e.target.value)
       }
+      fullWidth={fullWidth}
     />
   );
 }
