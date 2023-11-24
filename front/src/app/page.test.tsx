6d7b4cfaf5aabe39c7e0296d80bb98117fdae4fc
김태완 <aptakqmf12@gmail.com>
2023-11-24 18:11:47 +0900

    막걸리 리스트 json / test 스크립트 추가

--- front/src/app/page.test.tsx
+++ front/src/app/page.test.tsx
@@ -1,10 +1,11 @@
 import { screen, render } from "@testing-library/react";
 import Home from "./page";
+import { makgeolliList } from "@/mock/handler";
 
 test("최초 렌더링", () => {
   render(<Home />);
 
   const listBox = screen.getByRole("listbox");
 
-  expect(listBox).toHaveLength(7);
+  expect(listBox).toHaveLength(makgeolliList.length);
 });
